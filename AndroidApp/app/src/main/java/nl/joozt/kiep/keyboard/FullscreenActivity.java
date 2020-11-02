bf7868b89627e7820328f7396331629958e4ef98
markmutsaers <67520422+markmutsaers@users.noreply.github.com>
2020-11-02 21:33:10 +0100

    - Added TTS class to FullscreenActivity - Included some comments in FullscreenActivity ;-) - Updated TTS.java to play text to speech properly when hitting \ character (defined as variable) - Plays last line since previous . ! or ? In case first line it will play whole textbox

--- AndroidApp/app/src/main/java/nl/joozt/kiep/keyboard/FullscreenActivity.java
+++ AndroidApp/app/src/main/java/nl/joozt/kiep/keyboard/FullscreenActivity.java
@@ -18,20 +18,25 @@ public class FullscreenActivity extends AppCompatActivity {
     @SuppressLint("SourceLockedOrientationActivity")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set the content full screen & landscape
         setContentView(R.layout.activity_fullscreen);
-
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+        // Get the information of the editor where text is located
         editText = findViewById(R.id.editText);
         editText.setShowSoftInputOnFocus(false);
 
+        // Define instances on the yes/no functionality
         new YesNo(this, editText);
+
+        // Define instances of the TTS functionality
+        new TTS(this, editText);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
+        // Set properties of the UI
         findViewById(R.id.content).setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -39,12 +44,16 @@ public class FullscreenActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
+        // Get preferences with previously stored information
         SharedPreferences settings = getSharedPreferences(KIEP_KEYBOARD, Context.MODE_PRIVATE);
         String text = settings.getString("Text", KIEP_KEYBOARD);
+        // Set the text back in the editor
         editText.setText(text);
         if (text.equals(KIEP_KEYBOARD)) {
+            // If default text, select all and overwrite when start typing
             editText.selectAll();
         } else {
+            // Set the cursor to the end
             editText.setSelection(editText.getText().length());
         }
     }
@@ -52,9 +61,10 @@ public class FullscreenActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-
+        // Get settings and define preferences to store the text when minimizing / onPause
         SharedPreferences settings = getSharedPreferences(KIEP_KEYBOARD, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = settings.edit();
+        // Store the result in the preferences
         editor.putString("Text", editText.getText().toString());
         editor.commit();
     }
--- AndroidApp/app/src/main/java/nl/joozt/kiep/keyboard/TTS.java
+++ AndroidApp/app/src/main/java/nl/joozt/kiep/keyboard/TTS.java
@@ -1,4 +1,148 @@
 package nl.joozt.kiep.keyboard;
 
-public class TTS {
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.content.Context;
+import android.text.Editable;
+import android.text.SpannableStringBuilder;
+import android.text.TextWatcher;
+
+import java.util.HashMap;
+import java.util.Locale;
+import android.util.Log;
+import android.widget.EditText;
+
+public class TTS implements TextWatcher{
+    // Parameters used in the function
+    // - character to select speak
+    public static final String CHAR_SPEAK = "\\";
+    private static final String TAG = TTS.class.getSimpleName();
+
+    private TextToSpeech textToSpeech;
+    private final EditText editText;
+
+    // HashMap is included to use alternative pronunciation of words / characters
+    private HashMap<String, String> alternativePronunciations = new HashMap<String, String>();
+
+    public TTS(Context context, EditText editText) {
+        // Define variables to be used
+        this.editText = editText;
+
+        // Add a listener to the text
+        this.editText.addTextChangedListener(this);
+
+        // Alternative pronunciations list
+        alternativePronunciations.put("Y", "ei");
+
+        // Initialize TextToSpeech with the proper language setting (based on Locale)
+        textToSpeech = new TextToSpeech(context,
+                new OnInitListener() {
+
+                    @Override
+                    public void onInit(int status) {
+                        // Check if successfully loaded
+                        if (status == TextToSpeech.SUCCESS) {
+                            // Define the language to be used
+                            int result = textToSpeech.setLanguage(Locale.getDefault());
+                            // Check if language is loaded OK yes/no
+                            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                Log.e(TAG, "Language is not supported");
+                            } else {
+                                Log.i(TAG, "TextToSpeech is Initialized");
+                            }
+                        } else {
+                            // Display TTS is not able to initialize
+                            Log.e(TAG, "Failed to Initialize");
+                        }
+                    }
+                });
+    }
+
+    // Speak functionality with alternative pronunciation
+    public void speak(String text) {
+        // If no argument, return
+        if (textToSpeech == null || text == null || text.length() == 0) {
+            return;
+        }
+
+        // Check if text is equal to alternative pronunciation
+        // OI: to be checked for the complete string / list of words
+        String alternativePronunciation = alternativePronunciations.get(text);
+        if (alternativePronunciation != null) {
+            text = alternativePronunciation;
+        }
+
+        // Add message to logging
+        Log.i(TAG, "Speak: " + text);
+
+        // Speak text
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+
+    public void stop() {}
+
+    public void destroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+            textToSpeech = null;
+        }
+    }
+
+    // Check for last character and replace it if it is the one we're looking for
+    private boolean containsCharacter(Editable editable, String character) {
+        if (editable.toString().contains(character)) {
+            Editable ab = new SpannableStringBuilder(editable.toString().replace(character, ""));
+            editable.replace(0, editable.length(), ab);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        if (containsCharacter(s, CHAR_SPEAK)) {
+            // Get the total text from the edit box and trim leading / trailing spaces
+            String complete_text = this.editText.getText().toString();
+            complete_text = complete_text.trim();
+
+            // Remove the last character if it is dot, ! or ?
+            if (complete_text.charAt(complete_text.length()-1) == '.' || complete_text.charAt(complete_text.length()-1) == '!' || complete_text.charAt(complete_text.length()-1) == '?')
+                complete_text = complete_text.substring(0, complete_text.length()-1);
+
+            // Search for line ending characters, like dot, ! or ?
+            String last_sentence_dot  = complete_text.substring(complete_text.lastIndexOf(".")+1);
+            String last_sentence_expl = complete_text.substring(complete_text.lastIndexOf("!")+1);
+            String last_sentence_ques = complete_text.substring(complete_text.lastIndexOf("?")+1);
+
+            // Check for the shortest string length and play that case
+            int length_tot  = complete_text.length();
+            int length_dot  = last_sentence_dot.length();
+            int length_expl = last_sentence_expl.length();
+            int length_ques = last_sentence_ques.length();
+
+            // Check the minimal length
+            if (length_dot < length_expl && length_dot < length_ques && length_dot < length_tot)
+                // Speak last_sentence_dot
+                this.speak(last_sentence_dot);
+            if (length_expl < length_dot && length_expl < length_ques && length_expl < length_tot)
+                // Speak last_sentence_expl
+                this.speak(last_sentence_expl);
+            if (length_ques < length_dot && length_ques < length_expl && length_ques < length_tot)
+                // Speak last_sentence_ques
+                this.speak(last_sentence_ques);
+            if (length_tot <= length_dot && length_tot <= length_expl && length_tot <= length_ques)
+                // Speak the total buffer
+                this.speak(complete_text);
+        }
+    }
+
+    // Default overrides to make the TextWatcher work properly
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
 }
