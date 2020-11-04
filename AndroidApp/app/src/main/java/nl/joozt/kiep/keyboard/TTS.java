72e99e9561e6392f18b23a06495629d449b444cf
Joozt <github@joozt.nl>
2020-11-04 21:07:36 +0100

    Use KeyPressListener in TTS

--- AndroidApp/app/src/main/java/nl/joozt/kiep/keyboard/TTS.java
+++ AndroidApp/app/src/main/java/nl/joozt/kiep/keyboard/TTS.java
@@ -3,29 +3,20 @@ package nl.joozt.kiep.keyboard;
 import android.content.Context;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
-import android.text.Editable;
-import android.text.SpannableStringBuilder;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.widget.EditText;
 
 import java.util.Locale;
 
-public class TTS implements TextWatcher {
+public class TTS {
     // Parameters used in the function
     // - character to select speak
     public static final String CHAR_SPEAK = "\\";
     private static final String TAG = TTS.class.getSimpleName();
 
     private TextToSpeech textToSpeech;
-    private final EditText editText;
 
-    public TTS(Context context, EditText editText) {
-        // Define variables to be used
-        this.editText = editText;
-
-        // Add a listener to the text
-        this.editText.addTextChangedListener(this);
+    public TTS(Context context, final EditText editText) {
 
         // Initialize TextToSpeech with the proper language setting (based on Locale)
         textToSpeech = new TextToSpeech(context,
@@ -49,9 +40,16 @@ public class TTS implements TextWatcher {
                         }
                     }
                 });
+
+        KeyPressListener.listen(editText, CHAR_SPEAK, new KeyPressListener.OnKeyPressListener() {
+            @Override
+            public void onKeyPress() {
+                speak(getTextToSpeak(editText));
+            }
+        });
     }
 
-    // Speak functionality with alternative pronunciation
+    // Speak functionality
     public void speak(String text) {
         // If no argument, return
         if (textToSpeech == null || text == null || text.length() == 0) {
@@ -73,60 +71,41 @@ public class TTS implements TextWatcher {
         }
     }
 
-    // Check for last character and replace it if it is the one we're looking for
-    private boolean containsCharacter(Editable editable, String character) {
-        if (editable.toString().contains(character)) {
-            Editable ab = new SpannableStringBuilder(editable.toString().replace(character, ""));
-            editable.replace(0, editable.length(), ab);
-            return true;
+    private String getTextToSpeak(EditText editText) {
+        // Get the total text from the edit box and trim leading / trailing spaces
+        String complete_text = editText.getText().toString();
+        complete_text = complete_text.trim();
+
+        // Remove the last character if it is dot, ! or ?
+        if (complete_text.charAt(complete_text.length() - 1) == '.' || complete_text.charAt(complete_text.length() - 1) == '!' || complete_text.charAt(complete_text.length() - 1) == '?') {
+            complete_text = complete_text.substring(0, complete_text.length() - 1);
         }
-        return false;
-    }
 
-    @Override
-    public void afterTextChanged(Editable s) {
-        if (containsCharacter(s, CHAR_SPEAK)) {
-            // Get the total text from the edit box and trim leading / trailing spaces
-            String complete_text = this.editText.getText().toString();
-            complete_text = complete_text.trim();
+        // Search for line ending characters, like dot, ! or ?
+        String last_sentence_dot = complete_text.substring(complete_text.lastIndexOf(".") + 1);
+        String last_sentence_expl = complete_text.substring(complete_text.lastIndexOf("!") + 1);
+        String last_sentence_ques = complete_text.substring(complete_text.lastIndexOf("?") + 1);
 
-            // Remove the last character if it is dot, ! or ?
-            if (complete_text.charAt(complete_text.length() - 1) == '.' || complete_text.charAt(complete_text.length() - 1) == '!' || complete_text.charAt(complete_text.length() - 1) == '?')
-                complete_text = complete_text.substring(0, complete_text.length() - 1);
+        // Check for the shortest string length and play that case
+        int length_tot = complete_text.length();
+        int length_dot = last_sentence_dot.length();
+        int length_expl = last_sentence_expl.length();
+        int length_ques = last_sentence_ques.length();
 
-            // Search for line ending characters, like dot, ! or ?
-            String last_sentence_dot = complete_text.substring(complete_text.lastIndexOf(".") + 1);
-            String last_sentence_expl = complete_text.substring(complete_text.lastIndexOf("!") + 1);
-            String last_sentence_ques = complete_text.substring(complete_text.lastIndexOf("?") + 1);
+        // Check the minimal length
+        if (length_dot < length_expl && length_dot < length_ques && length_dot < length_tot)
+            // Speak last_sentence_dot
+            return last_sentence_dot;
+        if (length_expl < length_dot && length_expl < length_ques && length_expl < length_tot)
+            // Speak last_sentence_expl
+            return last_sentence_expl;
+        if (length_ques < length_dot && length_ques < length_expl && length_ques < length_tot)
+            // Speak last_sentence_ques
+            return last_sentence_ques;
+        if (length_tot <= length_dot && length_tot <= length_expl && length_tot <= length_ques)
+            // Speak the total buffer
+            return complete_text;
 
-            // Check for the shortest string length and play that case
-            int length_tot = complete_text.length();
-            int length_dot = last_sentence_dot.length();
-            int length_expl = last_sentence_expl.length();
-            int length_ques = last_sentence_ques.length();
-
-            // Check the minimal length
-            if (length_dot < length_expl && length_dot < length_ques && length_dot < length_tot)
-                // Speak last_sentence_dot
-                this.speak(last_sentence_dot);
-            if (length_expl < length_dot && length_expl < length_ques && length_expl < length_tot)
-                // Speak last_sentence_expl
-                this.speak(last_sentence_expl);
-            if (length_ques < length_dot && length_ques < length_expl && length_ques < length_tot)
-                // Speak last_sentence_ques
-                this.speak(last_sentence_ques);
-            if (length_tot <= length_dot && length_tot <= length_expl && length_tot <= length_ques)
-                // Speak the total buffer
-                this.speak(complete_text);
-        }
-    }
-
-    // Default overrides to make the TextWatcher work properly
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        return "";
     }
 }
