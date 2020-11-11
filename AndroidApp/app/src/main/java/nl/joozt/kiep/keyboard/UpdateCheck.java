3768372fe8000f09e77540ff541e7fbe3976175a
Joozt <github@joozt.nl>
2020-11-11 20:07:09 +0100

    Switch to flexible update

--- AndroidApp/app/src/main/java/nl/joozt/kiep/keyboard/UpdateCheck.java
+++ AndroidApp/app/src/main/java/nl/joozt/kiep/keyboard/UpdateCheck.java
@@ -4,15 +4,19 @@ import android.app.Activity;
 import android.content.IntentSender;
 import android.util.Log;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.play.core.appupdate.AppUpdateInfo;
 import com.google.android.play.core.appupdate.AppUpdateManager;
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.InstallState;
+import com.google.android.play.core.install.InstallStateUpdatedListener;
 import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.InstallStatus;
 import com.google.android.play.core.install.model.UpdateAvailability;
 import com.google.android.play.core.tasks.OnSuccessListener;
 import com.google.android.play.core.tasks.Task;
 
-public class UpdateCheck implements OnSuccessListener<AppUpdateInfo> {
+public class UpdateCheck implements OnSuccessListener<AppUpdateInfo>, InstallStateUpdatedListener {
     private static final String TAG = UpdateCheck.class.getSimpleName();
     private final Activity activity;
     private final AppUpdateManager updateManager;
@@ -33,21 +37,36 @@ public class UpdateCheck implements OnSuccessListener<AppUpdateInfo> {
     public void onSuccess(AppUpdateInfo appUpdateInfo) {
         if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
             startUpdate(appUpdateInfo);
+        } else if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+            Log.d(TAG, "Update downloaded, completing now");
+            popupSnackbarForCompleteUpdate();
         } else {
             Log.d(TAG, "No update available");
         }
     }
 
     private void startUpdate(AppUpdateInfo appUpdateInfo) {
-        if (!appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
-            Log.e(TAG, "Immediate update not allowed");
-            return;
-        }
-
+        Log.d(TAG, "Starting flexible update");
+        updateManager.registerListener(this);
         try {
-            updateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.IMMEDIATE, activity, 1);
+            updateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.FLEXIBLE, activity, 1);
         } catch (IntentSender.SendIntentException ignored) {
             Log.e(TAG, "Start update failed");
         }
+    }
+
+    @Override
+    public void onStateUpdate(InstallState state) {
+        if (state.installStatus() == InstallStatus.DOWNLOADED) {
+            Log.d(TAG, "Update downloaded, completing now");
+            updateManager.unregisterListener(this);
+            popupSnackbarForCompleteUpdate();
+        }
+    }
+
+    private void popupSnackbarForCompleteUpdate() {
+        Snackbar snackbar = Snackbar.make(activity.findViewById(R.id.content), R.string.update_ready, Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction(R.string.update_restart, view -> updateManager.completeUpdate());
+        snackbar.show();
     }
 }
