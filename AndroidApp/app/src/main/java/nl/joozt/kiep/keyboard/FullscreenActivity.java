9832a3e0c699adfdbe81cb497aac88a8c97865af
Joozt <github@joozt.nl>
2020-11-15 13:26:50 +0100

    Improve fullscreen

--- AndroidApp/app/src/main/java/nl/joozt/kiep/keyboard/FullscreenActivity.java
+++ AndroidApp/app/src/main/java/nl/joozt/kiep/keyboard/FullscreenActivity.java
@@ -59,13 +59,6 @@ public class FullscreenActivity extends AppCompatActivity implements SharedPrefe
     @Override
     protected void onResume() {
         super.onResume();
-        // Set properties of the UI
-        findViewById(R.id.content).setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
         // Get preferences with previously stored information
         SharedPreferences settings = getSharedPreferences(KIEP_KEYBOARD, Context.MODE_PRIVATE);
@@ -85,6 +78,17 @@ public class FullscreenActivity extends AppCompatActivity implements SharedPrefe
         }
 
         updateCheck.checkForUpdate();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
     @Override
